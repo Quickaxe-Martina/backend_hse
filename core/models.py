d632a5cbc8e3fcc1d6072863b6b6681594249327
Garun Furmanov <furmanov.garun@yandex.ru>
2023-03-10 23:27:46 +0300

    added_priority_Furmanov_Garun

--- core/models.py
+++ core/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 
 class TaskModel(models.Model):
+    objects = None
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
-    is_done = models.BooleanField(default=False)
+    is_done = models.BooleanField(default=False, verbose_name="Завершено")
+
+    class Priority(models.IntegerChoices):
+        low = 1
+        medium = 2
+        high = 3
+
+    priority = models.IntegerField(choices=Priority.choices, default=2)
+
+
+
