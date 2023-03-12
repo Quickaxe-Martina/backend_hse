c699763d67304390f527866baf954d4d866f2b5b
gplorovg <gplorovg@gmail.com>
2023-03-12 17:34:36 +0300

    fix priority in models

--- core/models.py
+++ core/models.py
@@ -10,4 +10,4 @@ class TaskModel(models.Model):
         ('M', 'Medium'),
         ('H', 'High'),
     )
-    priority = models.IntegerField(choices=PRIORITY, default='M')
+    priority = models.CharField(choices=PRIORITY, default='M')
