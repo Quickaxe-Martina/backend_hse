44be42826170cf112cb92cbb3a59ec9e98728835
Natalya-art <72634128+Natalya-art@users.noreply.github.com>
2023-03-12 23:03:13 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -5,3 +5,16 @@ class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
     is_done = models.BooleanField(default=False)
+    
+class Task(models.Model):
+    # другие поля модели
+    PRIORITY_CHOICES = [
+        ('high', 'Высокий'),
+        ('medium', 'Средний'),
+        ('low', 'Низкий')
+    ]
+    priority = models.CharField(
+        max_length=6,
+        choices=PRIORITY_CHOICES,
+        default='medium'
+    )
