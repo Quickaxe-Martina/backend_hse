937a4abdb8b505ead164ae6033c3de94aa6b7954
Natalya-art <72634128+Natalya-art@users.noreply.github.com>
2023-03-12 23:12:06 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -74,3 +74,16 @@ class TaskDetailAPIView(APIView):
     def delete(self, request, pk):
         TaskModel.objects.filter(pk=pk).delete()
         return Response(status=204)
+    
+    
+    class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(priority=request.data.get('priority', 'medium'))
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
