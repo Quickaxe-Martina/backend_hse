aaae30035495d26f3391254594f7866b4f65876d
Raaazzy <111382627+Raaazzy@users.noreply.github.com>
2023-03-12 03:14:10 +0300

    final version
    Ну вроде работает

--- core/models.py
+++ core/models.py
@@ -5,3 +5,9 @@ class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
     is_done = models.BooleanField(default=False)
+    SIZES = (
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'Hight'),
+    )
+    priority = models.CharField(max_length=1, verbose_name="Приоритет", choices=SIZES)
