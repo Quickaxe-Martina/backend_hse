a6f9c5dba26c6b50bd3b54c1cd24253bea59d376
gplorovg <gplorovg@gmail.com>
2023-03-12 17:57:42 +0300

    finished feature

--- core/models.py
+++ core/models.py
@@ -5,9 +5,5 @@ class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
     is_done = models.BooleanField(default=False)
-    PRIORITY = (
-        ('L', 'Low'),
-        ('M', 'Medium'),
-        ('H', 'High'),
-    )
-    priority = models.CharField(max_length=1, choices=PRIORITY, default='M')
+    priority_levels = models.TextChoices('priority_levels', 'L M H')
+    priority = models.CharField(max_length=1, blank=True, choices=priority_levels.choices, default='M')
