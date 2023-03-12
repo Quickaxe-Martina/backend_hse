20d36c451ab5d6b7ccd55cbfa1aba58e123d7d49
unknown <nastya.milowanowa2016@yandex.ru>
2023-03-12 20:45:13 +0300

    [feat] Добавлена новая фича: у задач есть теперь приоритет

--- core/models.py
+++ core/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
+    priority = models.CharField(
+        default='midium', max_length=6, verbose_name='Приоритет')
     is_done = models.BooleanField(default=False)
