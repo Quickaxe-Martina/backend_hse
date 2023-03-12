3c5d1354a6547587e547c6bb8690dd612a162422
gplorovg <gplorovg@gmail.com>
2023-03-12 17:10:09 +0300

    put priority in models

--- core/models.py
+++ core/models.py
@@ -5,3 +5,9 @@ class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
     is_done = models.BooleanField(default=False)
+    PRIORITY = (
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    )
+    priority = models.IntegerField(max_length=1, choices=PRIORITY)
