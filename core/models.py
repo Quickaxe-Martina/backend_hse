62354bc49f2ba04b871bb4e6f8d32db386dea561
unknown <nastya.milowanowa2016@yandex.ru>
2023-03-13 12:38:04 +0300

    [fix]: Исправлено поле приоритетов

--- core/models.py
+++ core/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
-    priority = models.CharField(max_length=6, default="midium", verbose_name="Приоритет")
+    PRIORITY_CHOICES = [("high", "Высокий"), ("midium",
+                                              "Средний"), ("low", "Низкий"),]
+    priority = models.CharField(
+        max_length=6, choices=PRIORITY_CHOICES, default="midium", verbose_name="Приоритет")
     is_done = models.BooleanField(default=False)
